89d25ea20e2a4d382cdc03f2f455fea486180d38
Carlo James Saladaga <saladagacarlojames@gmail.com>
2025-10-29 15:45:26 +0800

    Set max width for signup card

--- frontend/src/features/signup/SignupCard.tsx
+++ frontend/src/features/signup/SignupCard.tsx
@@ -3,7 +3,7 @@ function SignupCard() {
     return (
         <>
             <fieldset
-                className={`fieldset bg-base-100 border-base-300 rounded-box w-base border px-7 py-5 sm:px-15 sm:py-10 flex flex-col gap-5 shadow-2xl`}
+                className={`fieldset max-w-[376px] bg-base-100 border-base-300 rounded-box w-base border px-7 py-5 sm:px-15 sm:py-10 flex flex-col gap-5 shadow-2xl`}
             >
                 <h1 className="text-2xl font-bold text-center">
                     Welcome to Plantetto!
