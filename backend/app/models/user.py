f882b6e26b75fca7da55d8945246d54f5d735ecb
Carlo James Saladaga <saladagacarlojames@gmail.com>
2025-11-03 21:19:15 +0800

    Add more methods for auth in Users model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -25,6 +25,7 @@ class Users(UserMixin) :
 
     @classmethod
     def get_by_username(cls, username):
+        # does not include password
         db = get_db()
         cursor = db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
@@ -40,6 +41,28 @@ class Users(UserMixin) :
             uuid=result['uuid'],
             username=result['username'],
             email=result['email'],
+            created_at=result['created_at']
+        )
+
+    @classmethod
+    def get_for_auth(cls, username) :
+        # includes password, used only for auth
+        db = get_db()
+        cursor = db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+        sql = "SELECT * FROM users WHERE username = %s"
+
+        cursor.execute(sql, (username,))
+        result = cursor.fetchone()
+
+        if not result :
+            return None
+
+        return cls(
+            uuid=result['uuid'],
+            username=result['username'],
+            email=result['email'],
+            password=result['user_password'],
             created_at=result['created_at']
         )
 
@@ -74,3 +97,37 @@ class Users(UserMixin) :
         user = Users.get_by_email(email)
 
         return True if user else False
+
+    @classmethod
+    def get_by_id(cls, user_id) :
+        db = get_db()
+        cursor = db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+        sql = "SELECT * FROM users WHERE uuid= %s"
+
+        cursor.execute(sql, (user_id,))
+        result = cursor.fetchone()
+
+        if not result :
+            return None
+
+        return cls(
+            uuid=result['uuid'],
+            username=result['username'],
+            email=result['email'],
+            created_at=result['created_at']
+        )
+
+    def get_id(self) :
+        return str(self.uuid)
+
+    def check_password(self, password) : 
+        password_hash = hashlib.md5(password.encode()).hexdigest()
+        return password_hash == self.password
+    
+    def get_json(self) :
+        return {
+            'id':str(self.uuid),
+            'username':self.username,
+        }
+    
