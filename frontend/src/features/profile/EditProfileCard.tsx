3c6213c713ec09a6cb8c30d369ca968d2f298bed
Zjann Henry Cuajotor <zjannhenry.cuajotor@g.msuiit.edu.ph>
2025-11-04 09:47:25 +0800

    made EditProfileCard actually work (SetProfile connected)

--- frontend/src/features/profile/EditProfileCard.tsx
+++ frontend/src/features/profile/EditProfileCard.tsx
@@ -11,25 +11,31 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import SetProfile from "@/features/profile/SetProfile";
 
 export function DialogDemo() {
     return (
         <Dialog>
             <form>
                 <DialogTrigger asChild>
-                    <Button variant="outline">Open Dialog</Button>
+                    <Button variant="outline">Edit Profile</Button>
                 </DialogTrigger>
-                <DialogContent className="sm:max-w-[425px] bg-base-100">
+                <DialogContent className="sm:max-w-[600px] bg-base-100 max-h-[90vh] overflow-y-auto">
                     <DialogHeader>
-                        <DialogTitle>Edit profile</DialogTitle>
+                        <DialogTitle>Edit Profile</DialogTitle>
                         <DialogDescription>
-                            Make changes to your profile here. Click save when
-                            you&apos;re done.
+                            You can update your profile picture or name here.
                         </DialogDescription>
                     </DialogHeader>
-                    <div className="grid gap-4">
+                    <div className="grid gap-6">
+                        {/* prof */}
+                        <div className="flex justify-center">
+                            <SetProfile />
+                        </div>
+
+                        {/* name (add stuff later) */}
                         <div className="grid gap-3">
-                            <Label htmlFor="name-1">Name</Label>
+                            <Label htmlFor="name-1">Display Name</Label>
                             <Input
                                 id="name-1"
                                 name="name"
