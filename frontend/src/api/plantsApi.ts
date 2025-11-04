9a0c92181fd71310328bb9ab2a4a4e1d6e77b1e4
Zjann Henry Cuajotor <zjannhenry.cuajotor@g.msuiit.edu.ph>
2025-11-05 01:30:26 +0800

    created plantsApi functions to connect to plants endpoints

---
+++ frontend/src/api/plantsApi.ts
@@ -0,0 +1,64 @@
+import axios from "@/lib/axios";
+
+async function addPlant(formData: FormData) {
+    try {
+        const { data } = await axios.post("/plants", formData);
+
+        return { ok: true, plant: data };
+    } catch (error) {
+        console.error(error);
+        return { ok: false };
+    }
+}
+
+async function editPlant(plant_uuid: string, formData: FormData) {
+    try {
+        const { data } = await axios.put(`/plants/${plant_uuid}`, formData);
+
+        return { ok: true, plant: data };
+    } catch (error) {
+        console.error(error);
+        return { ok: false };
+    }
+}
+
+async function deletePlant(plant_uuid: string) {
+    try {
+        await axios.put(`/plants/${plant_uuid}`);
+
+        return { ok: true };
+    } catch (error) {
+        console.error(error);
+        return { ok: false };
+    }
+}
+
+async function fetchPlantsOfUser(username: string) {
+    try {
+        const { data } = await axios.get(`/user/${username}/plants/`);
+        const plants = data["plants"];
+        return { ok: true, plants: plants };
+    } catch (error) {
+        console.error(error);
+        return { ok: false };
+    }
+}
+
+async function fetchPlant(plant_uuid: string) {
+    try {
+        const { data } = await axios.get(`/plants/${plant_uuid}`);
+        const plant = data["plant"];
+        return { ok: true, plant: plant };
+    } catch (error) {
+        console.error(error);
+        return { ok: false };
+    }
+}
+
+export default {
+    addPlant,
+    editPlant,
+    deletePlant,
+    fetchPlantsOfUser,
+    fetchPlant,
+};
