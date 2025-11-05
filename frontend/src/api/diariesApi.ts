b41e6e23736c8cc4987beb396e95b7ac2166e171
Vincee Ezekiel M. Jandayan <vinceeezekiel.jandayan@g.msuiit.edu.ph>
2025-11-05 14:02:13 +0800

    Aded diary api functions in frontend

---
+++ frontend/src/api/diariesApi.ts
@@ -0,0 +1,78 @@
+import axios from "@/lib/axios";
+import type { PlantOptionType } from "@/features/garden/gardenTypes";
+import { isAxiosError } from "axios";
+import type {
+    DiaryCardType,
+    PlantDiaryType,
+} from "@/features/diary/diaryTypes";
+
+async function getUserPlantsOptions(username: string) {
+    try {
+        const { data } = await axios.get(`users/${username}/plants_options`);
+        const plantOptions: PlantOptionType[] = data["plant_options"];
+        return { ok: true, plantOptions: plantOptions };
+    } catch {
+        return { ok: false, plantOptions: [] };
+    }
+}
+
+async function addDiaryEntry(formData: FormData) {
+    try {
+        const { data } = await axios.post("/diaries/", formData);
+        const diary: PlantDiaryType = data["diary"];
+        return { ok: true, diary: diary };
+    } catch (error) {
+        console.error(error);
+        if (isAxiosError(error) && error.response) {
+            console.log(error.response.data.error);
+            return { ok: false, errors: error.response.data.error };
+        }
+        return { ok: false, errors: { root: "some error occurred" } };
+    }
+}
+
+async function editDiaryEntry(formData: FormData, diary_uuid: string) {
+    try {
+        const { data } = await axios.put(`/diaries/${diary_uuid}`, formData);
+        const diary: PlantDiaryType = data["diary"];
+        return { ok: true, diary: diary };
+    } catch (error) {
+        console.error(error);
+        if (isAxiosError(error) && error.response) {
+            console.log(error.response.data.error);
+            return { ok: false, errors: error.response.data.error };
+        }
+        return { ok: false, errors: { root: "some error occurred" } };
+    }
+}
+
+async function deleteDiaryEntry(diary_uuid: string) {
+    try {
+        const { data } = await axios.delete(`/diaries/${diary_uuid}`);
+        const diary: PlantDiaryType = data["diary"];
+        return { ok: true, diary: diary };
+    } catch (error) {
+        console.error(error);
+
+        return { ok: false };
+    }
+}
+
+async function fetchDiariesToday() {
+    try {
+        const { data } = await axios.get(`/diaries/`);
+        const plantDiaries: DiaryCardType[] = data["diaries"];
+        console.log(plantDiaries);
+        return { ok: true, plantDiaries: plantDiaries };
+    } catch {
+        return { ok: false, plantDiaries: [] };
+    }
+}
+
+export default {
+    getUserPlantsOptions,
+    addDiaryEntry,
+    editDiaryEntry,
+    deleteDiaryEntry,
+    fetchDiariesToday,
+};
