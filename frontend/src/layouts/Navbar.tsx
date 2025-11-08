fcfee1ce93ff9d456e8b0efa645bfa7beed1f419
Joshua Ray Haganas <joshuaray.haganas@g.msuiit.edu.ph>
2025-11-08 15:33:22 +0800

    Change Logout confirm button to btn-warning

--- frontend/src/layouts/Navbar.tsx
+++ frontend/src/layouts/Navbar.tsx
@@ -144,7 +144,7 @@ export default function Navbar() {
                             Cancel
                         </button>
                         <button
-                            className="btn btn-error"
+                            className="btn btn-warning "
                             onClick={handleConfirmLogout}
                         >
                             Log Out
