8d04d42a31d9f51d49b25ed1e24993f004c5fe3f
Joshua Ray Haganas <joshuaray.haganas@g.msuiit.edu.ph>
2025-11-11 21:35:44 +0800

    Add guides schema

---
+++ backend/schema/guides_schema.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS guides CASCADE;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE guides (
+    id BIGSERIAL PRIMARY KEY,
+    uuid UUID DEFAULT uuid_generate_v4() UNIQUE NOT NULL,
+    title VARCHAR(150) NOT NULL DEFAULT 'Untitled',
+    content JSONB NULL,
+    guide_status VARCHAR(10) CHECK (guide_status IN ('draft', 'published')) DEFAULT 'draft',
+    last_edit TIMESTAMPTZ NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    plant_id BIGSERIAL REFERENCES plant_types(id) ON DELETE CASCADE,
+    user_id BIGSERIAL REFERENCES users(id) ON DELETE CASCADE
+);
