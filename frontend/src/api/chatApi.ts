c1b0c9772759172c438e1ac41e809ffba3b231d2
Joshua Ray Haganas <joshuaray.haganas@g.msuiit.edu.ph>
2025-11-23 16:40:22 +0800

    Add getConversationRooms function in chatApi

--- frontend/src/api/chatApi.ts
+++ frontend/src/api/chatApi.ts
@@ -1,14 +1,27 @@
 import axios from "@/lib/axios";
+import type { ConversationRoomType } from "@/features/chat/chatTypes";
 
 async function getConversationRoom(username: string) {
     try {
-        const { data } = await axios.get(`chat/${username}`);
+        const { data } = await axios.get(`chat/room/${username}`);
         return { ok: true, conversationRoom: data["conversation_room"] };
     } catch {
         return { ok: false, conversationRoom: null };
     }
 }
 
+async function getConversationRooms() {
+    try {
+        const { data } = await axios.get(`chat/rooms`);
+        const conversationRooms: ConversationRoomType[] =
+            data["conversation_rooms"];
+        return { ok: true, conversationRooms: conversationRooms };
+    } catch {
+        return { ok: false, conversationRooms: [] };
+    }
+}
+
 export default {
     getConversationRoom,
+    getConversationRooms,
 };
